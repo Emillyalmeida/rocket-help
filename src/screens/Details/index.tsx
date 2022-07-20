4200a7ace827e9428ea07a727f2029c619f727b5
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-20 14:10:44 -0300

    feat: init screens Detailse Register

---
+++ src/screens/Details/index.tsx
@@ -0,0 +1,11 @@
+import { Heading, VStack } from "native-base";
+
+const Details = () => {
+  return (
+    <VStack>
+      <Heading>Detalhes</Heading>
+    </VStack>
+  );
+};
+
+export default Details;
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,11 @@
+import { Heading, VStack } from "native-base";
+
+const Register = () => {
+  return (
+    <VStack>
+      <Heading> Nova Solicitação</Heading>
+    </VStack>
+  );
+};
+
+export default Register;
