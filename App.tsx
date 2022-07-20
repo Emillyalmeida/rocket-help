1bb7b6236b5cff5f70f85b550be2e297976486e8
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-19 21:06:10 -0300

    feat: add font roboto

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import SignIn from "./src/screens/SignIn";
-import { NativeBaseProvider } from "native-base";
+import Loading from "./src/components/loading";
+
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { THEME } from "./src/styles/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        // backgroundColor="transparent"
+        // translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
